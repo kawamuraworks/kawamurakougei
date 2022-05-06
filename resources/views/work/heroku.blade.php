f00b5d5d3185c7febcb11650a73167d2ac97e69d
kawamura <dialoginthedark2000@outlook.jp>
2022-05-06 13:05:47 +0900

    Heroku使用時の実績画像表示修正

--- resources/views/work/heroku.blade.php
+++ resources/views/work/heroku.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <div class="card col-lg-10 k-card">
-        <img src="/works/work1/works-0001-1.jpg" class="card-img" alt="{{ $detail->headline }}">
+        <img src="/works/work1/works-0001-1.jpg" class="card-img"
+            alt="{{ $detail->headline }}">
         <div class="card-img-overlay">
             <h1 class="headline">{{ $detail->headline }}</h1>
             <p class="period">施工期間：{{ $detail->period }}</p>
@@ -24,11 +25,11 @@
             <tr>
                 <th>用　　　途</th>
                 <td>
-                    @for ($i = 0; $i < count($types); $i++)
-                        @if ($i == 0)
-                            {{ $types[$i]['name'] }}
-                        @elseif($i >= 1 && $types[$i]['num'] != 0)
-                            {{ '/ ' . $types[$i]['name'] }}
+                    @for($i=0; $i<count($types); $i++)
+                        @if($i==0)
+                        {{ $types[$i]['name'] }}
+                        @elseif($i>=1 && $types[$i]['num'] != 0)
+                        {{ '/ ' .$types[$i]['name'] }}
                         @endif
                     @endfor
                 </td>
@@ -37,11 +38,11 @@
             <tr>
                 <th>工 事 内 容</th>
                 <td>
-                    @for ($i = 0; $i < count($tags); $i++)
-                        @if ($i == 0)
-                            {{ $tags[$i]['name'] }}
-                        @elseif($i >= 1 && $tags[$i]['num'] != 0)
-                            {{ '/ ' . $tags[$i]['name'] }}
+                    @for($i=0; $i<count($tags); $i++)
+                        @if($i==0)
+                        {{ $tags[$i]['name'] }}
+                        @elseif($i>=1 && $tags[$i]['num'] != 0)
+                        {{ '/ ' .$tags[$i]['name'] }}
                         @endif
                     @endfor
                 </td>
@@ -72,7 +73,7 @@
                 @foreach ($images as $k => $v)
                     @if ($k == 0)
                         <div class="carousel-item active" data-bs-interval="5000">
-                            <img src="{{ '/works/work1/works-0001-' . $k + 1 . '.jpg' }}"
+                            <img src="{{ '/works/work1/works-0001-' . $k+1 . '.jpg' }}"
                                 alt="{{ $images[$k]->img_content }}" class="d-block k-slick-slide"
                                 style="margin: 0 auto;">
                             <div class="carousel-caption d-block k-carousel-caption">
@@ -82,7 +83,7 @@
                         @continue
                     @endif
                     <div class="carousel-item" data-bs-interval="5000">
-                        <img src="{{ '/works/work1/works-0001-' . $k + 1 . '.jpg' }}"
+                        <img src="{{ '/works/work1/works-0001-' . $k+1 . '.jpg' }}"
                             alt="{{ $images[$k]->img_content }}" class="d-block k-slick-slide"
                             style="margin: 0 auto;">
                         <div class="carousel-caption d-block k-carousel-caption">
@@ -108,17 +109,9 @@
                     <figure class="works-list k-select">
                         <a class="nav-link k-nav-link" aria-current="page"
                             href="/work/index?priority={{ $v->priority }}">
-                            @if ($k <= 2)
-                                <img src="{{ '/works/work2/works-0002-' . $k + 1 . '.jpg' }}" class="img-fluid"
-                                    alt="">
-                            @else
-                                <img src="{{ '/works/work2/works-0002-' . $k + 2 . '.jpg' }}" class="img-fluid"
-                                    alt="">
-                            @endif
-
-                            @if ($result == $v->priority)
-                                <p>選択中</p>
-                            @endif
+                            <img src="{{ '/works/work2/works-0002-' . $k+1 . '.jpg' }}"
+                                class="img-fluid" alt="">
+                            @if($result == $v->priority) <p>選択中</p> @endif
                         </a>
                         <figcaption class="lead">{{ $v->headline }}</figcaption>
                     </figure>
